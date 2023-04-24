24631628eb8b94d84eba8f7b5886c4b3b9d83405
vasa911 <vadym.vasylyev@outlook.com>
2023-04-24 10:57:16 +0200

    Add stub for UserIdentity

---
+++ BankSystem/BankSystem.Application/Services/UserIdentitySevice.cs
@@ -0,0 +1,19 @@
+﻿namespace BankSystem.Application.Services
+{
+    /// <summary>
+    /// Service for retrieving user data from token/cookie
+    /// </summary>
+    public class UserIdentitySevice : IUserIdentityService
+    {
+        /// <summary>
+        /// Retrieves User identificator
+        /// Just a stub for now
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Guid GetUserId()
+        {
+            return new Guid("797c5451-93e9-4af8-86e7-4b440172dea4");
+        }
+    }
+}
