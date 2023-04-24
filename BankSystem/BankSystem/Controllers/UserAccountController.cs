70a3d1b4d16c1008a51a3ea9bd5a56101b83ac9f
vasa911 <vadym.vasylyev@outlook.com>
2023-04-24 08:04:30 +0200

    Remove default project code; Add UserAccount controller

---
+++ BankSystem/BankSystem/Controllers/UserAccountController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserAccountController : ControllerBase
+    {
+
+        private readonly ILogger<UserAccountController> _logger;
+
+        public UserAccountController(ILogger<UserAccountController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
