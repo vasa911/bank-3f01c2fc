56cf39e23a6526d00d3c9da14c2b9ec408ac5800
vasa911 <vadym.vasylyev@outlook.com>
2023-04-24 10:58:19 +0200

    use fake userIdentity service to retrieve user id

--- BankSystem/BankSystem.Application/Commands/CreateUserAccount/CreateUserAccountCommandHandler.cs
+++ BankSystem/BankSystem.Application/Commands/CreateUserAccount/CreateUserAccountCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using BankSystem.Infrastructure.Repositories;
+﻿using BankSystem.Application.Services;
+using BankSystem.Infrastructure.Repositories;
 using MediatR;
 
 namespace BankSystem.Application.Commands.CreateUserAccount
@@ -6,15 +7,21 @@ namespace BankSystem.Application.Commands.CreateUserAccount
     public class CreateUserAccountCommandHandler : IRequestHandler<CreateUserAccountCommand, Guid>
     {
         private readonly IUserRepository _userRepository;
-        public CreateUserAccountCommandHandler(IUserRepository userRepository)
+        private readonly IUserIdentityService _userIdentityService;
+        public CreateUserAccountCommandHandler(
+            IUserRepository userRepository, 
+            IUserIdentityService userIdentityService)
         {
             _userRepository = userRepository;
+            _userIdentityService = userIdentityService;
         }
 
         public async Task<Guid> Handle(CreateUserAccountCommand request, CancellationToken cancellationToken)
         {
-            var id = Guid.NewGuid();
-            var user = await _userRepository.GetById(id);
+            var userId = _userIdentityService.GetUserId();
+            var user = await _userRepository.GetById(userId);
+            ArgumentNullException.ThrowIfNull(user);
+
             var newAccount = user.CreateAccount(request.Name);
             await _userRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
             return newAccount.Id;
